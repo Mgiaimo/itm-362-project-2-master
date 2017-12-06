3de723748ed98d9f2becf970dc69a30e87719023
Matteo Giaimo <mgiaimo@hawk.iit.edu>
2017-12-06 13:33:38 -0600

    final turn in
    made all the changes that Professor told me to make for the final turn in

--- js/js.js
+++ js/js.js
@@ -18,3 +18,12 @@ function helpfunction() {
     }
     document.getElementById("help").innerHTML = txt;
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
